79c367001655bedc8aafdba760254ce80d2c8681
Mr-Pranjal2051 <56365654+Mr-Pranjal2051@users.noreply.github.com>
2022-04-02 23:44:20 +0530

    Create Print_Variation.py

---
+++ Python/Print_Variation.py
@@ -0,0 +1,9 @@
+print("Today is a good day to continue learning python.")
+print("It's fairly easy.")
+print('It just requires a lot of "practice.')
+print("Anyone can learn it"+'. '+"and there's no prior knowledge requirement.")
+greeting="Hello"
+name1="PERSEUS"
+print(greeting+","+" "+name1+'!')
+name=input("Please enter a name: ")
+print(greeting+" "+name+"!")

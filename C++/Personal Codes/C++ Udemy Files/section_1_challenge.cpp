0394f1837cb6c52ce841f88e6ee593142cef9512
Mr-Pranjal2051 <56365654+Mr-Pranjal2051@users.noreply.github.com>
2022-08-01 01:00:55 +0530

    Add files via upload

---
+++ C++/Personal Codes/C++ Udemy Files/section_1_challenge.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    int a;
+    std::cout<<"Enter your favorite number between 1 and 100: "<<std::endl;
+    std::cin>>a;
+    std::cout<<"Amazing!! That's my favorite number too!\n";
+    std::cout<<"No, really!!, "<<a<<" is my favorite number!\n";
+    return 0;
+}
